9f95b8c2ac113ab2fc7569c98e8d1fe459f2b194
ikloerzer <ikloerzer@106-23.info2>
2023-02-08 13:45:17 +0100

    C1

---
+++ Tema5_Entrega/src/buscaminas/Funciones.java
@@ -0,0 +1,19 @@
+package buscaminas;
+
+public class Funciones {
+	
+	//Declaramos las variables y los arrays necesarios.
+	
+	final int TAM= 20;
+	
+	public static int[] dibujaTablero {
+		
+		//Hacemos el array unidimensional que será el tablero.
+		int []tabla= new int[TAM];
+		
+		
+		//Devolvemos la tabla.
+		return tabla;
+	}
+
+}

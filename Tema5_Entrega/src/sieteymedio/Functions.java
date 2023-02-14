b71c8dd187a8e8efcb312ce4acc8bea6f2a43417
ikloerzer <ikloerzer@106-23.info2>
2023-02-14 12:45:22 +0100

    Ejercicio 2 casi terminado, falta comentarlo todo bien

--- Tema5_Entrega/src/sieteymedio/Functions.java
+++ Tema5_Entrega/src/sieteymedio/Functions.java
@@ -3,112 +3,108 @@ package sieteymedio;
 public class Functions {
 
 	// Declaramos las variables y los arrays necesarios.
-	
-	static final double MAX_PUNTOS=7.5;
-	
-	
-	public static double[] rellenaMazo (double[] mazo) {
-		
-		for (int i=0; i<mazo.length;i++) {
-			
-			mazo[i]=i+1;
-			
-			if (i>=7) {
-				
-				mazo[i]=0.5;
-			} 
-			
+
+	static final double MAX_PUNTOS = 7.5;
+
+	public static double[] rellenaMazo(double[] mazo) {
+
+		for (int i = 0; i < mazo.length; i++) {
+
+			mazo[i] = i + 1;
+
+			if (i >= 7) {
+
+				mazo[i] = 0.5;
+			}
+
 		}
-		
+
 		return mazo;
-		
+
 	}
 
-	public static double randomCartas(double [] mazo) {
-		
+	public static double randomCartas(double[] mazo) {
+
 		double valor;
 		int index;
 
-		index = (int) (Math.random()* 9 )+1;
-		
-		valor=mazo[index];
-		
+		index = (int) (Math.random() * 9) + 1;
+
+		valor = mazo[index];
+
 		return valor;
-	
+
 	}
-	
-	public static String conversionCarta (double valor) {
-		
-		String conversion="";
-		
+
+	public static String conversionCarta(double valor) {
+
+		String conversion = "";
+
 		switch ((int) valor) {
-			
+
 		case 1:
-			
-			conversion="el as";
+
+			conversion = "el as";
 			break;
-			
-		case 2: 
-			
-			conversion="dos";
+
+		case 2:
+
+			conversion = "dos";
 			break;
-			
-		case 3: 
-			
-			conversion="tres";
-		break;
-		
+
+		case 3:
+
+			conversion = "tres";
+			break;
+
 		case 4:
-			
-			conversion="cuatro";
+
+			conversion = "cuatro";
 			break;
-			
+
 		case 5:
-			
-			conversion="cinco";
+
+			conversion = "cinco";
 			break;
-			
+
 		case 6:
-			
-			conversion="seis";
+
+			conversion = "seis";
 			break;
-			
+
 		case 7:
-			
-			conversion="siete";
+
+			conversion = "siete";
 			break;
-		
+
 		}
-		
-		if (valor==0.5) {
-			
+
+		if (valor == 0.5) {
+
 			int random;
-			random= (int)(Math.random()*3)+1;
-			
+			random = (int) (Math.random() * 3) + 1;
+
 			switch (random) {
-			
-			case 1: 
-				
-				conversion= "el sota ";
+
+			case 1:
+
+				conversion = "el sota ";
 				break;
-				
+
 			case 2:
-				
-				conversion="el caballo ";
+
+				conversion = "el caballo ";
 				break;
-				
-			case 3: 
-				
-				conversion= "el rey ";
+
+			case 3:
+
+				conversion = "el rey ";
 				break;
-			
+
 			}
-			
-			
+
 		}
-		
-		
-		
+
 		return conversion;
 	}
 
@@ -116,7 +112,7 @@ public class Functions {
 
 		int index;
 
-		index = (int) (Math.random()* 4)+1;
+		index = (int) (Math.random() * 4) + 1;
 
 		String valorPalo = "";
 
@@ -147,18 +143,57 @@ public class Functions {
 		return valorPalo;
 
 	}
-	
-	public static boolean fin(double valor) {
+
+	public static boolean fin(double valorJ1, double valorJ2) {
 
 		boolean fin = false;
 
-		if () {
+		if (valorJ2 >= MAX_PUNTOS) {
 
-			pierde = true;
+			fin = true;
 		}
 
-		return pierde;
+		if (valorJ1 > MAX_PUNTOS && valorJ2 > MAX_PUNTOS) {
+
+			// Ambos jugadores pierden porque ambos se han pasado de 7.5
+			fin=true;
+		}
+
+		return fin;
 	}
 
+	public static void ganador(double valorJ1, double valorJ2) {
+
+		if (valorJ1 > valorJ2 && valorJ1 < MAX_PUNTOS && valorJ2 < MAX_PUNTOS) {
+
+			// Gana el jugador 1.
+			System.out.println("Ha ganado el jugador 1.");
+
+		}
+		
+		if (valorJ1<=MAX_PUNTOS &&valorJ2>MAX_PUNTOS) {
+			
+			System.out.println("Ha ganado Jugador 1.");
+		}
+
+		if (valorJ2 > valorJ1 && valorJ1 < MAX_PUNTOS && valorJ2 < MAX_PUNTOS) {
+
+			// Gana el jugador 2.
+			System.out.println("Ha ganador el jugador 2.");
+		}
+
+		if (valorJ2 == valorJ1 && valorJ1 < MAX_PUNTOS && valorJ2 < MAX_PUNTOS) {
+
+			// Ha habido empate y ninguno se ha pasado.
+			System.out.println("Ha habido empate.");
+		}
+
+		if (valorJ1 > MAX_PUNTOS && valorJ2 > MAX_PUNTOS) {
+
+			// Ambos jugadores pierden porque ambos se han pasado de 7.5
+			System.out.println("Han perdido ambos jugadores.");
+		}
+
+	}
 
 }
--- Tema5_Entrega/src/sieteymedio/Main.java
+++ Tema5_Entrega/src/sieteymedio/Main.java
@@ -10,28 +10,27 @@ public class Main {
 	public static void main(String[] args) {
 
 		// Declaramos los arrays y las variables necesarias.
-		double[] mazo= new double[10]; //Mazo de cartas.
-		
-		
+		double[] mazo = new double[10]; // Mazo de cartas.
+
 		String carta; // Array que nos sacará un valor aleatorio.
 		String respuesta; // Variable que va a introducir el usuario.
 		double puntosJ1 = 0;
-		double sumaPuntosJ1=0;
-		double sumaPuntosJ2=0;
+		double sumaPuntosJ1 = 0;
+		double sumaPuntosJ2 = 0;
 		double puntosJ2 = 0;
 		String palo;
-		boolean fin = false;
+		boolean fin = true;
 
 		// Creamos un escáner.
 		Scanner sc = new Scanner(System.in);
 
 		// Presentamos el juego.
 		System.out.println("Bienvenido/a, se va a iniciar el juego del siete y medio.\n");
-		
+
 		System.out.println(Arrays.toString(Functions.rellenaMazo(mazo)));
 
 		// Esto tiene que ir en un bucle while más grande.
-		while (!fin) {
+		while (fin) {
 
 			System.out.println("Es el turno del jugador1. ¿Quiere sacar una carta? (s/n): ");
 			respuesta = sc.next();
@@ -39,37 +38,32 @@ public class Main {
 			if (respuesta.equals("s")) {
 
 				do {
-					
-					
+
 					// Hay que presentar la carta.
-					
+
 					puntosJ1 = Functions.randomCartas(mazo);
-					carta= Functions.conversionCarta( puntosJ1 );
+					carta = Functions.conversionCarta(puntosJ1);
 					palo = Functions.valorPalo();
-					
+
 					System.out.println("Tu carta es: " + carta + palo);
-							
+
 					// Sumamos los puntos.
-					
-					sumaPuntosJ1+=puntosJ1;
-					
+
+					sumaPuntosJ1 += puntosJ1;
+
 					System.out.println("El jugador 1 tiene " + sumaPuntosJ1 + " puntos.");
-					
+
 					// Comprobamos si nos pasamos de la puntuación.
-					if (sumaPuntosJ1>=Functions.MAX_PUNTOS) {
-						
-						//Indicar al usuario que ha perdido.
-						System.out.println("Vaya, te has pasado, sorry");
-						
-						//Forzamos la salida del bucle
+					if (sumaPuntosJ1 > Functions.MAX_PUNTOS) {
+
+						// Indicar al usuario que ha perdido.
+						System.out.println("Vaya, te has pasado, sorry.");
 						break;
-						
+
 					}
-					
+
 					System.out.println("¿Quiere sacar otra carta? (s/n): ");
 					respuesta = sc.next();
-
-					
 
 				} while (respuesta.equals("s"));
 
@@ -81,41 +75,45 @@ public class Main {
 			if (respuesta.equals("s")) {
 
 				do {
-	
-					
+
 					// Hay que presentar la carta.
 					puntosJ2 = Functions.randomCartas(mazo);
 					carta = Functions.conversionCarta(puntosJ2);
 					palo = Functions.valorPalo();
 
 					System.out.println("Tu carta es: " + carta + palo);
-					
-					sumaPuntosJ2+=puntosJ2;
+
+					sumaPuntosJ2 += puntosJ2;
 
 					System.out.println("El jugador 2 tiene " + sumaPuntosJ2 + " puntos.");
-					
-					// Aquí hay que pedir una fumnción para comprobar si se ha pasado.
-					if (sumaPuntosJ1>Functions.MAX_PUNTOS) {
-						
-						//Indicar al usuario que ha perdido.
-						System.out.println("Vaya, te has pasado, sorry");
-						
-						//Forzamos la salida del bucle
+
+					// Aquí hay que pedir una función para comprobar si se ha pasado.
+					if (sumaPuntosJ2 > Functions.MAX_PUNTOS) {
+
+						// Indicar al usuario que ha perdido.
+						System.out.println("Vaya, te has pasado, sorry.");
 						break;
-						
+
 					}
 
 					System.out.println("¿Quiere sacar otra carta? (s/n): ");
 					respuesta = sc.next();
 
 				} while (respuesta.equals("s"));
-				
-				
-				//Aquí ponemos la función fin.
 
+				// Aquí pido la función en caso de que el jugador 2 haya perdido.
+				fin = Functions.fin(sumaPuntosJ1, sumaPuntosJ2);
+			
 			}
+			
+			// en caso de que no haya perdido ninguno de los jugadores, comparamos los
+			// resultados.
+			Functions.ganador(sumaPuntosJ1, sumaPuntosJ2);
 
 		}
+
+		
+	
 
 	}
 
